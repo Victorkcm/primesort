666c55562bbb88e2c86de5ed52ed3d9ecfb44f86
Victor Augusto Souza Adao - 160258 <ra160258@le20-10.grad.fee.unicamp.br>
2017-11-09 19:31:47 -0200

    primesortdefinitivo

--- src/main.c
+++ src/main.c
@@ -1,17 +1,64 @@
-/* Contador de palavras
- *
- * Este programa recebera uma serie de caracteres representando palavras em sua
- * entrada. Ao receber um caractere fim de linha ('\n'), deve imprimir na tela o
- * numero de palavras separadas que recebeu e, apos, encerrar.
- */
-
 #include <stdio.h>
+#include <stdlib.h>
+ 
 
-int main() {
+int primo (int x)
+{
+int n=x,i, div=0;
+if (n==2)
+{
+return 1;
+}
 
-  int x, y;
+if (n==0 || n==1)
+{
+return 2;
+} 
 
-  scanf("%d %d\n", &x, &y);
-  printf("%d\n", x + 200);
+else
+{
+	i=1;
+	while (i<n+1)
+	{
+	if (n%i==0)
+		{
+		div++;
+		}
+	if (div>2)
+		{
+		return 2;
+		}
+	i++;	
+	}
+
+}
+}
+
+int main()
+{	
+  int v,j,f;
+  FILE *pipe;
+  
+  pipe=popen("sort -n -r", "w");
+
+  for (j=0;;j++) {
+
+    scanf ("%d", &v);
+
+    if(v<0)
+      break;
+  
+    else
+	{
+      	f=primo(v);	
+      	if(f==2)
+		{
+        	fprintf (pipe, "%d\n", v);
+      		}
+    	}
+  }	
+
+  pclose (pipe);
+
   return 0;
 }
